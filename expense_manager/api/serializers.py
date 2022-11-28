460c86c343cef191af6c5a8b6ee536d7dbde24b3
XenoProxy <ksenomorph6@gmail.com>
2022-11-29 00:22:41 +0300

    [+] user views

--- expense_manager/api/serializers.py
+++ expense_manager/api/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import UserManager
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from django.utils.six import BytesIO
+
 
 from models import Category
 
@@ -18,16 +16,3 @@ class CategorySerializer(serializers.Serializer):
 
 serializer = CategorySerializer(Category.category)
 serializer.data
-
-# сериализация объектов
-json = JSONRenderer().render(serializer.data)
-json
-
-# десериализация объектов
-stream = BytesIO(json)
-data = JSONParser().parse(stream)
-# затем мы сохраняем эти нативные типы данных в словарь валидных данных.
-serializer = CategorySerializer(data=data)
-serializer.is_valid()
-# True
-serializer.validated_data
--- expense_manager/api/views.py
+++ expense_manager/api/views.py
@@ -1,29 +1,13 @@
 # Для создания эндпоинта только для чтения, который возвращал бы список пользователей
 from rest_framework import generics
 from . import serializers
-from rest_framework import viewsets
 from django.contrib.auth.models import User
 
 
-from serializers import UserSerializer
+class UserViewSet(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-
-# class UserList(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = serializers.UserSerializer
-
-
-# class UserDetail(generics.RetrieveAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = serializers.UserSerializerrender
-
-# # Create your views here.
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
