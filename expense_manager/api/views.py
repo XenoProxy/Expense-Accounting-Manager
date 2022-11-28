b905520409f19413f81dff32de5d9cd5de2e4bfd
XenoProxy <ksenomorph6@gmail.com>
2022-11-28 20:42:19 +0300

    users view set

--- expense_manager/api/views.py
+++ expense_manager/api/views.py
@@ -1,16 +1,29 @@
 # Для создания эндпоинта только для чтения, который возвращал бы список пользователей
 from rest_framework import generics
 from . import serializers
+from rest_framework import viewsets
 from django.contrib.auth.models import User
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
+from serializers import UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = serializers.UserSerializerrender
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
 
-# Create your views here.
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+# class UserList(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = serializers.UserSerializer
+
+
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = serializers.UserSerializerrender
+
+# # Create your views here.
