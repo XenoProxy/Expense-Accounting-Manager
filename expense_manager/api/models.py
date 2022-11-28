b524b0594196c39aaec1c21c80284f0b19f35c26
XenoProxy <ksenomorph6@gmail.com>
2022-11-29 01:24:33 +0300

    [+] category serializer

--- expense_manager/api/models.py
+++ expense_manager/api/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Category(models.Model):
     type = models.CharField(max_length=100, blank=True, default='')
+    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
     class Meta:
         ordering  = ['type']
--- expense_manager/api/serializers.py
+++ expense_manager/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from .models import Category
 # ссылочная связь считается более правильной при проектировании API
 class UserSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
-            model = User
-            fields = ('id', 'username', 'email')
+        model = User
+        fields = ('id', 'username', 'category')
 
+class CategorySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    class Meta:
+        model = Category
+        fields = ['id', 'type', 'owner']
